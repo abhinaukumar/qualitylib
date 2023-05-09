30cf1267259d259cb96dee31243978c2ad0e4fc4
Abhinau Kumar <ab.kumr98@gmail.com>
2023-05-08 23:32:39 -0700

    Reflecting new submodule names

--- qualitylib/tools/__init__.py
+++ qualitylib/tools/__init__.py
@@ -1,3 +1,3 @@
-from .imresize import imresize
-from .read_dataset import read_dataset
-from .import_python_file import import_python_file
+from .resize import imresize
+from .dataset import read_dataset
+from .import_python import import_python_file
