23a61364a29a29096dcd1608c7aab6cc2755e528
Abhinau Kumar <ab.kumr98@gmail.com>
2023-05-09 22:08:02 -0700

    Initial commit of setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='qualitylib',
+    author='Abhinau Kumar',
+    author_email='ab.kumr98@gmail.com',
+    version='0.1.0',
+    url='https://github.com/abhinaukumar/qualitylib',
+    description='Package for seamlessly running quality assessment experiments in Python.',
+    install_requires=[
+        'joblib'
+        'numpy',
+        'scipy',
+        'scikit-learn',
+        'videolib @ git+https://github.com/abhinaukumar/videolib@main'
+    ],
+    python_requires='>=3.7.0',
+    license='MIT License',
+    packages=find_packages()
+)
